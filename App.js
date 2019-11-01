7356a63518e65ce73fc1d60c42b96de24b5fa7ee
Keji <keji.amos@guildeducation>
2019-11-01 12:11:14 -0600

    render different tabs

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 import React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import HomeScreen from './HomeScreen';
+import {HomeScreen} from './HomeScreen';
 
 const MainNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
--- PeopleScreen.js
+++ PeopleScreen.js
@@ -1,8 +1,25 @@
 import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
 import SegmentedControlIOS from '@react-native-community/segmented-control';
-export const HomeScreen = ({navigation}) => {
+
+export const PeopleScreen = ({navigation}) => {
   const [tab, setTabs] = useState(0);
+  const handleTabPress = () => {
+    if (tab === 0) {
+      console.error('hit me');
+      return (
+        <View>
+          <Text>PEOPLE 34</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View>
+          <Text>PEOPLE 36</Text>
+        </View>
+      );
+    }
+  };
 
   return (
     <View>
@@ -10,16 +27,8 @@ export const HomeScreen = ({navigation}) => {
         values={['34', '36']}
         selectedIndex={tab}
         onChange={event => setTabs(event.nativeEvent.selectedSegmentIndex)}
+        onValueChage={handleTabPress}
       />
-      {tab === 0 ? (
-        <View>
-          <Text>FLOOR 34</Text>
-        </View>
-      ) : (
-        <View>
-          <Text>FLOOR 36</Text>
-        </View>
-      )}
 
       {/* <Button
         title="Go to Jane's profile"
