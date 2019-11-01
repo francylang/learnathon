2bc22a454715166306bf087dfbb8ff098d9575aa
Keji <keji.amos@guildeducation>
2019-11-01 11:31:20 -0600

    add segemented control

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Button} from 'react-native';
-
+import {Button, View} from 'react-native';
+import SegmentedControlIOS from '@react-native-community/segmented-control';
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Welcome',
@@ -8,10 +8,13 @@ class HomeScreen extends React.Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigate('Profile', {name: 'Jane'})}
-      />
+      <View>
+        <SegmentedControlIOS values={['34', '36']} />
+        <Button
+          title="Go to Jane's profile"
+          onPress={() => navigate('Profile', {name: 'Jane'})}
+        />
+      </View>
     );
   }
 }
